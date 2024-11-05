8eec114a2d50d436f520b6106b5016f2598f1315
Koni Marti <koni.marti@gmail.com>
2024-11-05 16:48:47 +0100

    more work

--- test.sh
+++ test.sh
@@ -31,3 +31,22 @@ sku = 284758393
 
 color = "gray"
 EOF
+
+c3c compile-run ./example/toml-test.c3 . <<EOF
+[offset-date-time]
+odt1 = 1979-05-27T07:32:00Z
+odt2 = 1979-05-27T00:32:00-07:00
+odt3 = 1979-05-27T00:32:00.999999-07:00
+odt4 = 1979-05-27 07:32:00Z
+
+[local-date-time]
+ldt1 = 1979-05-27T07:32:00
+ldt2 = 1979-05-27T00:32:00.999999
+
+[local-date]
+ld1 = 1979-05-27
+
+[local-time]
+lt1 = 07:32:00
+lt2 = 00:32:00.999999
+EOF
